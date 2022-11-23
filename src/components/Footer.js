bc0572df27afd9918ff6b4c65c8c2937e3cb903c
ramosw1993 <ramosw1993@gmail.com>
2022-11-22 23:09:58 -0500

    footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,25 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "../styles/Footer.css";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+
+
+// We use JSX curly braces to evaluate the style object
 
 function Footer() {
-    return "Hello";
-  }
-  
-  export default Footer;
+  return (
+    <div className="d-flex align-items-center justify-content-center footer">
+      <a href="https://github.com/ramosw1993">
+        <h1>
+          <BsGithub/>
+        </h1>
+      </a>
+      <a href="https://www.linkedin.com/in/william-ramos-rosario-a06992248/">
+        <h1>
+          <BsLinkedin/>
+        </h1>
+      </a>
+    </div>
+  );
+}
+
+export default Footer;
